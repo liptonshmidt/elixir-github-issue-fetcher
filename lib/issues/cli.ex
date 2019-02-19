8dd8324c90afb169ab9947bb8c487e22dec3a8b4
Alexey Lipnyagov <liptonshmidt@gmail.com>
2019-02-19 14:41:46 +0300

    Add HTTPoison from hex

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -9,7 +9,9 @@ defmodule Issues.CLI do
 
 
   def run(argv) do
-    parse_args(argv)
+    argv 
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -20,8 +22,8 @@ defmodule Issues.CLI do
   """
   def parse_args(argv) do
     OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
-      |> elem(1)
-      |> args_to_internal_representation()
+    |> elem(1)
+    |> args_to_internal_representation
   end
 
   def args_to_internal_representation([user, project, count]) do
@@ -34,5 +36,17 @@ defmodule Issues.CLI do
 
   def args_to_internal_representation(_) do
     :help
+  end
+
+  def process(:help) do
+    IO.puts """
+      usage:  issues <user> <project> [ count | #{@default_count} ]
+    """
+
+    System.halt(0)
+  end
+
+  def process([user, project, count]) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
