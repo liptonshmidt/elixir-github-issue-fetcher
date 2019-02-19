fb4533223c52dd31a1ef4f2ec5e8e6a825bf3834
Alexey Lipnyagov <liptonshmidt@gmail.com>
2019-02-19 18:25:31 +0300

    Add desc sort and selection of only :count issues

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -2,7 +2,7 @@ defmodule CliTest do
   use ExUnit.Case
   doctest Issues
 
-  import Issues.CLI, only: [parse_args: 1]
+  import Issues.CLI, only: [parse_args: 1, sort_into_desc_order: 1]
 
   test "greets the world" do
     assert Issues.hello() == :world
@@ -19,5 +19,18 @@ defmodule CliTest do
 
   test "set default count if 2 values given" do
     assert parse_args(["user", "project"]) == {"user", "project", 4}
+  end
+
+  test "sorts into desc order correctly" do
+    result = sort_into_desc_order(fake_created_at_list([3, 1, 2]))
+
+    issues = for issue <- result, do: Map.get(issue, "created_at")
+
+    assert issues == [3, 2, 1]
+  end
+
+  defp fake_created_at_list(values) do
+    for value <- values,
+    do: %{"created_at" => value, "other_data" => "data"}
   end
 end
